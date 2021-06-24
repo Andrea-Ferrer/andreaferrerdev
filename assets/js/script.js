40fb675c2dc4f287ba759a680986e58dd821dc5b
Andrea <tellmeferrer@gmail.com>
2021-06-24 00:18:42 -0500

    correcciones

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,25 +15,36 @@ flechaIzquierda.addEventListener("click", () =>{
     fila.scrollLeft -= fila.offsetWidth;
 });
 
-//Segundo slider
 /*
-const filaDos = document.querySelector (".contenedor-slider-dos");
-const sliderDos = document.querySelector (".card-slider-dos");
-
-const flechaIzquierdaDos = document.getElementById ("flecha-izquierda-dos");
-const flechaDerechaDos = document.getElementById ("flecha-derecha-dos");
-
-//Event Listener para flecha derecha
-flechaDerechaDos.addEventListener("click", () =>{
-    fila.scrollLeft += fila.offsetWidth;
-});
-
-//Event Listener para flecha izquierda
-flechaIzquierdaDos.addEventListener("click", () =>{
-    fila.scrollLeft -= fila.offsetWidth;
+//Hover primer slider
+slider.forEach(() => {
+    slider.addEventListener("click", (e) => {
+        const elemento = e.currentTarget;
+        setTimeout (() => {
+            slider.forEach(card => card.classList.remove("hover"));
+            elemento.classList.add("hover");
+        }, 300);
+    });
 });
 */
 
+//Segundo slider
+/*Interaccion de los botones del slider*/
+const hilera = document.querySelector("#contSlide");
+const contenido = document.querySelector("#cardSlide");
+
+const arrowLeft = document.getElementById("arrowLeft");
+const arrowRight = document.getElementById("arrowRight");
+
+//event listener flecha izquierda
+arrowLeft.addEventListener("click", () => {
+    hilera.scrollLeft -= hilera.offsetWidth
+    console.log("Hola amigos");
+});
+arrowRight.addEventListener("click", () =>{
+    hilera.scrollLeft += hilera.offsetWidth;
+    console.log("Adios amigos");
+});
 
 
 
